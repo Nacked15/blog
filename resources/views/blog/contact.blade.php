a379d192f006fc89c403ef7dbb8fccf817f35612
Luis Yama <jluis.yama@gmail.com>
2017-06-03 11:20:44 -0600

    Upload project files

---
+++ resources/views/blog/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/main')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h3 class="text-center">Contact me</h3>
+        <div class="col-md-8">
+
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+    </div>
+</div>
+@endsection
